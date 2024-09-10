5d8d65c2bac4d7c935089743d50cf791958f8f93
b4ldous <baldous@protonmail.com>
2024-09-10 15:21:27 -0600

    Setup GitHub Actions for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    distDir: 'out',
-    images: {
-      unoptimized: true,
-    },
-  };
+  output: 'export', // Habilita la exportación estática
+  basePath: '/portfolio-minimal', // Cambia por el nombre exacto de tu repositorio
+  assetPrefix: '/portfolio-minimal', // Nombre exacto del repositorio
+  images: {
+    unoptimized: true, // GitHub Pages no soporta imágenes optimizadas por Next.js
+  },
+};
 
 export default nextConfig;
